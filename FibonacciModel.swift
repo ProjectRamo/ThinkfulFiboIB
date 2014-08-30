bdd67cf944c0853e39d9841bd31f5a594c443ae2
projectramo <projectramo@gmail.com>
2014-08-29 21:55:00 -0500

    Initial commit

---
+++ FibonacciModel.swift
@@ -0,0 +1,25 @@
+//
+//  FibonacciModel.swift
+//  Fibonacci
+//
+//  Created by Omar Haneef on 8/21/14.
+//  Copyright (c) 2014 Omar Haneef. All rights reserved.
+//
+
+import Foundation
+
+class FibonacciModel {
+    
+    func calculateFibonacciNumbers (minimum2 endOfSequence:Int) -> Array<Int> {
+        //Declaring a stored property with initial values
+        var sequence : [Int] = [1,1]
+        for number in 2..<endOfSequence {
+            
+            var newFibonacciNumber = sequence[number-1] + sequence[number-2]
+            sequence.append(newFibonacciNumber)
+        }
+        return sequence
+    }
+    
+}
+
--- IBFibonacci/ViewController.swift
+++ IBFibonacci/ViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    @IBOutlet weak var theSlider: UISlider!
+    @IBOutlet weak var selectedValueLabel: UILabel!
+    @IBOutlet weak var outputTextView: UITextField!
+    
+    //An instance of the model
+    var fibo: FibonacciModel = FibonacciModel()
+    
+    @IBAction func sliderValueDidChange(sender: AnyObject) {
+    }
+    func sliderValueDidChange () {
+        
+        var returnedArray: [Int] = []
+        var formattedOutput:String = ""
+        
+        //Display the updated slider value
+        self.selectedValueLabel!.text = String(Int(theSlider!.value))
+        
+        //Calculate the Fibonacci elements based on the new slider value
+        returnedArray = self.fibo.calculateFibonacciNumbers(minimum2: Int(theSlider!.value))
+        
+        //Put the elements in a nicely formatted array
+        for number in returnedArray {
+            
+            formattedOutput = formattedOutput + String(number) + ", "
+        }
+        
+        //Update the textfield with the formatted array
+        self.outputTextView!.text = formattedOutput
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
